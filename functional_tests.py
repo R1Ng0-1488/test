02794f972cf92df63c973c742762aeaf93e36021
R1Ng0-1488 <t1m.sadist@gmail.com>
2020-10-08 16:56:58 +0500

    expanded the dirst FT in comments, now using unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,33 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+	"""new visitor's test"""
+
+	def setUp(self):
+		"""installation"""
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		"""demontaj"""
+		# close browser
+		self.browser.quit()
+
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		"""test: can start a list and get it later"""
+		# Эдит слышала про крутое новое онлайн-приложение со
+		# списком неотложных дел. Она решает оценить его
+		# домашнюю страницу
+		self.browser.get('http://localhost:8000')
+
+		# Она видит, что заголовок и шапка страницы говорят о
+		# списках неотложных дел
+
+		self.assertIn('Django', self.browser.title)
+		self.fail(self.browser.title)
+		# comments...
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
