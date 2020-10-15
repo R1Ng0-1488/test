54f4bd8d3a67ab264abdb8662f5d9185c5ff1949
R1Ng0-1488 <57253219+R1Ng0-1488@users.noreply.github.com>
2020-10-15 17:51:33 +0500

    refractorisation view of home page for using template

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -8,16 +9,7 @@ from lists.views import home_page
 class HomePageTest(TestCase):
 	"""home page test"""
 
-	def test_root_url_resolves_to_home_page_view(self):
-		"""test: root url chenge into view of home page"""
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
-
-	def test_home_page_returns_correct_html(self):
-		"""test: home page returns correct html"""
-		request = HttpRequest()
-		response = home_page(request)
-		html = response.content.decode('utf8')
-		self.assertTrue(html.startswith('<html>'))
-		self.assertIn('<title>To-Do lists</title>', html)
-		self.assertTrue(html.endswith('</html>'))
+	def test_uses_home_template(self):
+		"""test: use home template"""
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'home.html')
