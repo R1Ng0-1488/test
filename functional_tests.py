92401db49421162fef7862e111793473880a4836
R1Ng0-1488 <57253219+R1Ng0-1488@users.noreply.github.com>
2020-10-15 17:05:27 +0500

    FT is checking possibility enter element of list

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -24,10 +26,31 @@ class NewVisitorTest(unittest.TestCase):
 
 		# Она видит, что заголовок и шапка страницы говорят о
 		# списках неотложных дел
-		print(self.browser.title, 'To-Do')
 		self.assertIn('To-Do', self.browser.title)
-		self.fail(self.browser.title)
-		# comments...
+		# test header
+		header_text = self.browser.find_element_by_tag_name('h1').text 
+		self.assertIn('To-Do', header_text)
+		# test placeholder of inputbox
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+			)
+		# test 
+		# Она набирате в текстовом поле "Купить павлиные перья"
+		inputbox.send_keys('Купить павлиные крылья')
+		# Когда она нажимает энтер страница обновляется, и теперь страница
+		# содержит "1: Купить павлиньи перья" в качестве элемента списка
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep()
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Купить павлиные крылья' for row in rows)
+		)
+		self.fail('Finish the test!')
+
 
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
