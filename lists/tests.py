86345a8060bcd5200807474abcdbc62732ce7bc7
R1Ng0-1488 <57253219+R1Ng0-1488@users.noreply.github.com>
2020-10-21 16:21:53 +0500

    Переадресация после ПОСТ и показ всех элементов в шаблоне

--- lists/tests.py
+++ lists/tests.py
@@ -16,11 +16,33 @@ class HomePageTest(TestCase):
 		self.assertTemplateUsed(response, 'home.html')
 
 	def test_can_save_a_POST_request(self):
-		"""test: can save a post-request"""
-		response = self.client.post('/', data={'item_text': 'A new list item'})
-		self.assertIn('A new list item', response.content.decode())
-		self.assertTemplateUsed(response, 'home.html')
+		"""test: can save a post-request """
+		self.client.post('/', data={'item_text': 'A new list item'})
 
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+
+	def test_redirects_after_POST(self):
+		'''test: redirect after POST'''
+		response = self.client.post('/', data={'item_text': 'A new list item'})
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/')
+
+	def test_only_saves_items_when_necessaru(self):
+		"""test: save elements when it needs"""
+		self.client.get('/')
+		self.assertEqual(Item.objects.count(), 0)
+
+	def test_display_all_list_items(self):
+		'''test: display all list items'''
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+
+		response = self.client.get('/')
+
+		self.assertIn('itemey 1', response.content.decode())
+		self.assertIn('itemey 2', response.content.decode())
 
 class ItemModelTest(TestCase):
 	'''тест модели элемента списка'''
