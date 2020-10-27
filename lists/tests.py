c87130a6cff75e42f4c770f67b28eab7e7a05418
R1Ng0-1488 <57253219+R1Ng0-1488@users.noreply.github.com>
2020-10-27 15:52:41 +0500

    add list.html change tests

--- lists/tests.py
+++ lists/tests.py
@@ -27,22 +27,13 @@ class HomePageTest(TestCase):
 		'''test: redirect after POST'''
 		response = self.client.post('/', data={'item_text': 'A new list item'})
 		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/')
+		self.assertEqual(response['location'], '/lists/one-list-in-the-world/')
 
 	def test_only_saves_items_when_necessaru(self):
 		"""test: save elements when it needs"""
 		self.client.get('/')
 		self.assertEqual(Item.objects.count(), 0)
 
-	def test_display_all_list_items(self):
-		'''test: display all list items'''
-		Item.objects.create(text='itemey 1')
-		Item.objects.create(text='itemey 2')
-
-		response = self.client.get('/')
-
-		self.assertIn('itemey 1', response.content.decode())
-		self.assertIn('itemey 2', response.content.decode())
 
 class ItemModelTest(TestCase):
 	'''тест модели элемента списка'''
@@ -64,3 +55,22 @@ class ItemModelTest(TestCase):
 		second_saved_item = saved_items[1]
 		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
 		self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+class ListViewTest(TestCase):
+	'''test: view of list'''
+
+	def test_uses_list_template(self):
+		'''test: using list template'''
+		response = self.client.get('/lists/one-list-in-the-world/')
+		self.assertTemplateUsed(response, 'list.html')
+
+	def test_displays_all_items(self):
+		'''test: displays all items of list'''
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+
+		response = self.client.get('/lists/one-list-in-the-world/')
+
+		self.assertContains(response, 'itemey 1')
+		self.assertContains(response, 'itemey 2')
