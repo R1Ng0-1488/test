e8a40837c96afb9b17e3d8c92ed96e97bb7f5a23
R1Ng0-1488 <57253219+R1Ng0-1488@users.noreply.github.com>
2020-10-30 13:09:37 +0500

    changed urls

--- lists/urls.py
+++ lists/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from lists import views
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home_page, name='home'),
-    path('lists/', include('lists.urls')),
+    path('new', views.new_list, name='new_list'),
+    path('<int:pk>/', views.view_list, name='view_list'),
+    path('<int:pk>/add_item', views.add_item, name='add_item'),
 ]
