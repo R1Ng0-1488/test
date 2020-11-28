96dcd7ed5382c7d62981cada5555497ab6a89592
R1Ng0-1488 <57253219+R1Ng0-1488@users.noreply.github.com>
2020-11-28 14:09:24 +0500

    changed settings

--- superlists/settings.py
+++ superlists/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'database/db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
     }
 }
 
@@ -120,6 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'static'))
-static_dirs = [STATIC_URL]
-print( BASE_DIR)
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+# static_dirs = [STATIC_URL]
+# print( BASE_DIR)
